1753e0dc5b34c018aef65c8744e037fbd56523a2
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2013-05-04 14:32:38 +0300

    initial release

---
+++ example.php
@@ -0,0 +1,8 @@
+<?php
+
+// simple usage example
+
+
+include "emoticons.php";
+
+echo smiley("This :) is a nice feature :D to have on your app. ;)");
---
+++ list.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//the list of the smilyes
+
+include "emoticons.php";
+
+
+echo smiley("", 1);
+
+
+// echo smiley("", 1, 5);
